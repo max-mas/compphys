567e399bf83f8ee7edfbe997803f986d780dc706
Max Maschke <m.maschke@tu-bs.de>
2024-03-20 16:47:08 +0100

    implemented coefficient matrix, be wary of results tho!

--- alpha_decay/src/main.py
+++ alpha_decay/src/main.py
@@ -15,8 +15,13 @@ M_alpha = 3727.379 #  alpha particle mass (MeV)
 
 
 def main():
-    a = Alphadecay(1, 1, 1, 1, 1, 1)
-    print(a.test())
+    rng = get_coulomb_range(92, 238)
+    a = Alphadecay(92, 238, rng, 10)
+    print(a.E_kin)
+    print(a.piecewise_constant_potential())
+    A = a.coeff_mat()
+    print(np.real(A))
+    print(A.shape)
     return
 
 
@@ -27,24 +32,112 @@ class Alphadecay:
     Z: int
     R: float
     V0: float
+    E_kin: float
     discr_steps: int
     coulomb_rng: float    
     mass_dict: mass_dict_type
 
     # exact coulomb
-    # Z1, Z2: charge numbers
     # r: distance (fm)
     # return: energy (MeV)
-    def coulomb(self, Z1, Z2, r) -> float:
+    def coulomb(self, r) -> float:
+        Z1 = self.Z - 2
+        Z2 = 2
         return fs * Z1 * Z2 * hc / r
-    
-    # TODO rm
-    def test(self) -> float:
-        return self.mass_dict[(90, 234)]
+
+    # E kin from mass defect in the frame where the daughter nucleus is stationary
+    # return: energy (MeV)
+    def E_kin_alpha(self):
+        M_1 = self.mass_dict[(self.Z, self.A)]
+        M_2 = self.mass_dict[(self.Z-2, self.A-4)]
+        E_defect = M_1 - M_2 - M_alpha
+
+        v_rel_squared = 2 * E_defect * (M_2 + M_alpha) / (M_2 * M_alpha) # (c)
+        E_alpha = 0.5 * v_rel_squared * M_alpha 
+
+        return E_alpha
+
 
     # TODO function that generates the discretised potential
+    def piecewise_constant_potential(self):
+        V = np.empty(self.discr_steps + 2, dtype=np.double)
+        dx = (self.coulomb_rng - self.R) / self.discr_steps # TODO does R factor in here?
+
+        V[0] = self.V0
+        for i in range(1, self.discr_steps + 2):
+            x = self.R + (i-1) * dx
+            V[i] = self.coulomb(x)
+        
+        return V
+    
+    # TODO k / kappa
+    def k(self, V):
+        return np.sqrt(2 * M_alpha * np.abs(self.E_kin - V)) / hc
+    
+    def kappa(self, V):
+        return np.sqrt(2 * M_alpha * np.abs(V - self.E_kin)) / hc
+
 
     # TODO function that makes the matrix for the LSE
+    def coeff_mat(self):
+        xs = np.ones(self.discr_steps + 1, dtype=np.double) * self.R 
+        dx = (self.coulomb_rng - self.R) / self.discr_steps
+        for i in range(self.discr_steps + 1):
+            xs[i] += i * dx
+
+        V = self.piecewise_constant_potential()
+        
+        A = np.zeros((2 * self.discr_steps + 3, 2 * self.discr_steps + 3), dtype=np.complex64)
+        A[0, 0] = 1
+        x = xs[0]
+        V1 = V[0]
+        V2 = V[1]
+        A[1, 0] =  np.exp( 1j * self.k(V1) * x)
+        A[1, 1] =  np.exp(-1j * self.k(V1) * x)
+        A[1, 2] = -np.exp( self.kappa(V2) * x)
+        A[1, 3] = -np.exp(-self.kappa(V2) * x)
+        A[2, 0] =  1j * self.k(V1) * np.exp( 1j * self.k(V1) * x)
+        A[2, 1] = -1j * self.k(V1) * np.exp(-1j * self.k(V1) * x) 
+        A[2, 2] = -     self.kappa(V2) * np.exp( self.kappa(V2) * x) 
+        A[2, 3] =       self.kappa(V2) * np.exp(-self.kappa(V2) * x) 
+
+        print(0, x)
+
+        for i in range(3, 2*self.discr_steps + 1):
+            j = (i-1) // 2 
+            #if j > self.discr_steps - 1:
+            #    break
+            V1 = V[j]
+            V2 = V[j + 1]
+            x = xs[j]
+            print(j, x, i)
+                     
+            if i % 2 == 1:
+                A[i, 2*j    ] =  np.exp( self.kappa(V1) * x)
+                A[i, 2*j + 1] =  np.exp(-self.kappa(V1) * x)
+                A[i, 2*j + 2] = -np.exp( self.kappa(V2) * x)
+                A[i, 2*j + 3] = -np.exp(-self.kappa(V2) * x)
+            else:
+                A[i, 2*j]       =  self.kappa(V1) * np.exp( self.kappa(V1) * x)
+                A[i, 2*j + 1]   = -self.kappa(V1) * np.exp(-self.kappa(V1) * x) 
+                A[i, 2*j + 2]   = -self.kappa(V2) * np.exp( self.kappa(V2) * x) 
+                A[i, 2*j + 3]   =  self.kappa(V2) * np.exp(-self.kappa(V2) * x) 
+            
+        x = xs[-1]
+        V1 = V[-2]
+        V2 = V[-1]
+        A[-2, -3] =  np.exp( self.kappa(V1) * x)
+        A[-2, -2] =  np.exp(-self.kappa(V1) * x)
+        A[-2, -1] = -np.exp( 1j * self.k(V2) * x)
+        A[-1, -3] =   self.kappa(V1) * np.exp( self.kappa(V1) * x) 
+        A[-1, -2] = - self.kappa(V1) * np.exp(-self.kappa(V1) * x)
+        A[-1, -1] = -1j * self.k(V2) * np.exp( 1j * self.k(V2) * x)
+
+        print("end", x)
+        print(self.kappa(V1), x, self.kappa(V1)*x)
+
+        return A
+
 
     # TODO function that solves the LSE and returns the coefficients
 
@@ -60,19 +153,41 @@ class Alphadecay:
     # V0: Depth of core well wrt. alpha particle energy TODO (unit)
     # discr_steps: Number of discrete steps to divide the potential into
     # coulomb_rng: Range of the coulomb potential (fm) (set to zero beyond)
-    def __init__(self, A, Z, R, V0, discr_steps=100, coulomb_rng=10): #TODO change default range to sensible value
-        self.A = A
-        self.Z = Z
-        self.R = R
-        self.V0 = V0
-        self.discr_steps = discr_steps
-        self.coulomb_rng = coulomb_rng
-
+    def __init__(self, Z, A, coulomb_rng, discr_steps=100):
         # TODO dictionary with atomic masses for mother and daughter nuclei
         self.mass_dict = Dict.empty(key_type=mass_dict_key_type, value_type=float64) # key: (Z, A)
         self.mass_dict[(92, 238)] = 221742.9 # U 238
         self.mass_dict[(90, 234)] = 218010.23 # Th 234
 
+        self.Z = Z
+        self.A = A
+        self.R = 1.3 * A ** (1/3) # TODO vary this?
+        self.V0 = - 134 # MeV
+        self.E_kin = self.E_kin_alpha()
+        self.discr_steps = discr_steps
+        self.coulomb_rng = coulomb_rng
+
+        
+def coulomb_minus_E(r, E, Z1, Z2):
+        return fs * Z1 * Z2 * hc / r - E
+
+def E_kin_alpha(Z, A):
+        mass_dict = dict() # key: (Z, A)
+        mass_dict[(92, 238)] = 221742.9 # U 238
+        mass_dict[(90, 234)] = 218010.23 # Th 234
+        M_1 = mass_dict[(Z, A)]
+        M_2 = mass_dict[(Z-2, A-4)]
+        E_defect = M_1 - M_2 - M_alpha
+
+        v_rel_squared = 2 * E_defect * (M_2 + M_alpha) / (M_2 * M_alpha) # (c)
+        E_alpha = 0.5 * v_rel_squared * M_alpha 
+
+        return E_alpha
+
+def get_coulomb_range(Z, A):
+    E = E_kin_alpha(Z, A)
+    x = scp.optimize.fsolve(coulomb_minus_E, x0=1, args=(E, Z-2, 2))
+    return x # TODO ??
 
 if __name__ == "__main__":
     main()
