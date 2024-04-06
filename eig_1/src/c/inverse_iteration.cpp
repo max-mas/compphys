ae3689e9e05dd7b7dce1b0e91e10bb224129b0ec
Max Maschke <m.maschke@tu-bs.de>
2024-04-06 20:17:30 +0200

    updated gitignore

---
+++ eig_1/src/c/inverse_iteration.cpp
@@ -0,0 +1,5 @@
+//
+// Created by max on 4/6/24.
+//
+
+#include "inverse_iteration.h"
---
+++ eig_1/src/c/inverse_iteration.h
@@ -0,0 +1,8 @@
+//
+// Created by max on 4/6/24.
+//
+
+#ifndef EIG1_INVERSE_ITERATION_H
+#define EIG1_INVERSE_ITERATION_H
+
+#endif //EIG1_INVERSE_ITERATION_H
